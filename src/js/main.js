3c3519091e47c63ad57c46e4977fabc2d619a04e
Lareta <lareta064@gmail.com>
2020-07-30 18:02:50 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
+	// Показать форму по клику на гамбургер
+	const gamburgerBtn = document.querySelector('.btn-bars');
+	const modalForm = document.querySelector('#feedback');
 
 })
